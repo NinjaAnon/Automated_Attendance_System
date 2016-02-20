c9419cfd2311eb99b45e23f4a41bd44a29014879
Anubhav <pandeyanubhav415@gmail.com>
2016-02-20 17:35:52 +0530

    Register Code Updated

--- AASYS/src/java/Register.java
+++ AASYS/src/java/Register.java
@@ -41,3 +41,48 @@ public class Register extends HttpServlet {
         String UID = request.getParameter("uid");
         String pass = request.getParameter("pass");
         
+        try{
+        
+        //loading drivers for mysql
+        Class.forName("com.mysql.jdbc.Driver");
+
+	//creating connection with the database 
+          Connection  con=DriverManager.getConnection
+                     ("jdbc:mysql://localhost:3306/aasys","root","apple123");
+
+        PreparedStatement ps=con.prepareStatement
+                  ("insert into student_employees values(?,?,?)");
+
+        ps.setString(1, name);
+        ps.setString(2, pass);
+        ps.setString(3, UID);
+        int i=ps.executeUpdate();
+        
+          if(i>0)
+          {
+            out.println("You are sucessfully registered");
+          }
+        
+        }
+        catch(Exception se)
+        {
+            se.printStackTrace();
+        }
+	
+      }
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
