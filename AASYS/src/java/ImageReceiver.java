e10aff77f362e8cb7cb892c39ceea8d094baefc0
Anubhav <pandeyanubhav415@gmail.com>
2016-03-15 14:19:06 +0530

    Image receiver added

---
+++ AASYS/src/java/ImageReceiver.java
@@ -0,0 +1,39 @@
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.Random;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import sun.misc.BASE64Decoder;
+
+@WebServlet(name = "ImageReceiver", urlPatterns = {"/receiver"})
+
+public class ImageReceiver extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            StringBuffer buffer = new StringBuffer();
+            Reader reader = request.getReader();
+            int current;
+
+            while ((current = reader.read()) >= 0) {
+                buffer.append((char) current);
+            }
+
+            String data = new String(buffer);
+            data = data.substring(data.indexOf(",") + 1);
+
+            System.out.println("PNG image data on Base64: " + data);
+            int name = new Random().nextInt(100000);
+            FileOutputStream output = new FileOutputStream(new File("C:\\Users\\Anubhav_Pandey\\Bitnami Trac Stack projects\\Automated Attendance System\\AttendanceSheets\\"
+                    + name + ".png"));
